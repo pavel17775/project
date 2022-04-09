edd07028d24a1882ef7a88d3551350efe473d641
Pavel <example@gmail.com>
2022-04-09 12:23:12 +0300

    practice part 1 and added file .gitignore

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log(1);
+'use strict';
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели','');
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},  
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотренных фильмов',''),
+      b = prompt('на сколько оцените фильм',''),
+      c = prompt('один из последних просмотренных фильмов',''),
+      d = prompt('на сколько оцените фильм','');
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+console.log(personalMovieDb);
